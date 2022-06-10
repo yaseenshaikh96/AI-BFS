25fa28ad339064a64a141f35b7c3667ccae6a618
yaseen <shaikhyaseen6496@gmail.com>
2022-06-11 00:25:05 +0530

    fragmented graph

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,12 @@
-﻿public class Program
+﻿public static class Program
 {
     public static void Main(string[] args)
     {
         Graph graph = new Graph();
+        Graph.Node A = graph.AddNode("A");
+        Graph.Node B = graph.AddNode("B", A);
+        Graph.Node C = graph.AddNode("C", B);
+
+        graph.DeleteNode(A);
     }
 }
