0e886a57f2d45ae05d0027d61911ad46c99de173
yaseen <shaikhyaseen6496@gmail.com>
2022-06-10 16:35:27 +0530

    initial

---
+++ src/WaterJug.cs
@@ -0,0 +1,13 @@
+public class WaterJug
+{
+    int jug1Cap, jug2Cap, target;
+
+    public WaterJug(int jug1Cap, int jug2Cap, int target)
+    {
+        this.jug1Cap = jug1Cap;
+        this.jug2Cap = jug2Cap;
+        this.target = target;
+    }
+
+
+}
